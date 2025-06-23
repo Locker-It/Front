aa6a4ea5371a2761d99cbec0598980950225c364
OfekSagiv <ofeksagiv@gmail.com>
2025-06-23 23:48:18 +0300

    fixed issues

--- src/components/shared/Logo/Logo.jsx
+++ src/components/shared/Logo/Logo.jsx
@@ -5,7 +5,8 @@ import { getLogoStyles } from './logoVariants';
 import { LOGO_VARIANTS } from '../../../constants/types';
 
 const Logo = ({ variant = LOGO_VARIANTS.DEFAULT, ...props }) => {
-  const src = getLogoStyles()[variant]?.src;
+  const styles = getLogoStyles();
+  const src = styles[variant]?.src || styles[LOGO_VARIANTS.DEFAULT].src;
   return (
     <StyledLogo variant={variant} src={src} alt="LockerIt Logo" {...props} />
   );
